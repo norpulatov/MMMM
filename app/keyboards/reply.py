21b43e4ecb427cc55b3b1d25f13133f2b6f29d9e
norpulatov <101542393+norpulatov@users.noreply.github.com>
2026-04-09 18:01:47 +0500

    Update reply.py

--- app/keyboards/reply.py
+++ app/keyboards/reply.py
@@ -1,11 +1,11 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+#from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 
-def main_menu_keyboard() -> ReplyKeyboardMarkup:
-    return ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="🎬 Kinolar ro'yxati"), KeyboardButton(text="🔍 Qidirish")],
-            [KeyboardButton(text="ℹ️ Bot haqida")],
-        ],
-        resize_keyboard=True,
-    )
+#def main_menu_keyboard() -> ReplyKeyboardMarkup:
+ #   return ReplyKeyboardMarkup(
+#        keyboard=[
+ #           [KeyboardButton(text="🎬 Kinolar ro'yxati"), KeyboardButton(text="🔍 Qidirish")],
+ #           [KeyboardButton(text="ℹ️ Bot haqida")],
+  #      ],
+  #      resize_keyboard=True,
+#    )
